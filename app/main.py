723f87d6451200d7d55d8b7c8ad62c9af6e0494d
franjefriten <fj.friastenza@gmail.com>
2025-01-31 17:35:24 +0100

    Implementation between model and app created

--- app/main.py
+++ app/main.py
@@ -1,28 +1,33 @@
-from fastapi import FastAPI, Response, Request
-from flask import Flask, make_response, redirect, render_template, request
-import uvicorn
-
-app = FastAPI(debug=True)
+from flask import Flask, make_response, redirect, render_template, request, url_for
+import pickle
+import os
 
 flask_app = Flask(__name__)
 
 @flask_app.route('/')
 def redirect_to_home():
-    response = make_response(redirect('/home'))
-    return response
+    return redirect(url_for('home'))
 
 @flask_app.route('/home')
 def home():
     return render_template("home.html")
 
-@app.post('/translate-text')
-def translate(user_text):
-    pass
-
 @flask_app.route('/process-text', methods=["POST"])
 def process_text():
+    # Get the text from the form input
     user_text = request.form.get('text')
-    pass
+    
+    # Process the text (you can perform any logic here)
+    if user_text:
+        with open(os.path.join(os.getcwd(), "model/modelo.pkl"), 'rb') as modelo_file:
+            cv, le, modelo = pickle.load(file=modelo_file)
+        predictions = le.inverse_transform(modelo.predict(cv.transform([user_text])))[0]
+        response_message = f"Idioma predicho: {predictions}"
+    else:
+        response_message = "No text received"
+    
+    # Pass the message to the template and render the page with the message
+    return render_template('home.html', response_message=response_message)
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+if __name__ == '__main__':
+    flask_app.run(host="localhost", port=8000, debug=True)
